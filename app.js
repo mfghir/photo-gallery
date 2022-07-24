29d362953a8728e0a11338db8f538af16cc18e90
miss-f-g <fatemeghafari98@gmail.com>
2022-07-24 13:44:06 +0430

    6.write generatePictures func

--- app.js
+++ app.js
@@ -27,28 +27,32 @@ async function fetchApi(url) {
   return data;
 }
 
-async function curatedPhotos() {
-  const data = fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
-
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img> <p>${photo.photographer}</p>`;
+    galleryImg.innerHTML = `
+        <div class="gallery-info">
+            <p>${photo.photographer}</p>
+            <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src=${photo.src.large}></img>`;
     gallery.appendChild(galleryImg);
   });
 }
 
+async function curatedPhotos() {
+  const data = await fetchApi(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1"
+  );
+  generatePictures(data);
+}
+
 async function searchPhotos(query) {
-  const data = fetchApi(
+  const data = await fetchApi(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
-
-  data.photos.forEach((photo) => {
-    const galleryImg = document.createElement("div");
-    galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img> <p>${photo.photographer}</p>`;
-    gallery.appendChild(galleryImg);
-  });
+  generatePictures(data);
 }
 
 curatedPhotos();
