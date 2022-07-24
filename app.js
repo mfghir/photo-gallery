b81034f89fc02cc7a9f4a0e7342a28e065181de8
miss-f-g <fatemeghafari98@gmail.com>
2022-07-24 13:24:36 +0430

    4.write searchPhotos func

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 let searchValue;
 
+
+
+
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    searchPhotos(searchValue);
+  });
+
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+
 async function curatedPhotos() {
   const dataFetch = await fetch(
     "https://api.pexels.com/v1/curated?per_page=15&page=1",
@@ -27,5 +42,29 @@ async function curatedPhotos() {
     gallery.appendChild(galleryImg);
   });
 }
+
+
+async function searchPhotos(query) {
+    const dataFetch = await fetch(
+        `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: auth,
+        },
+      }
+    );
+  
+    const data = await dataFetch.json();
+    console.log(data);
+  
+    data.photos.forEach((photo) => {
+      const galleryImg = document.createElement("div");
+      galleryImg.classList.add("gallery-img");
+      galleryImg.innerHTML = `<img src=${photo.src.large}></img> <p>${photo.photographer}</p>`;
+      gallery.appendChild(galleryImg);
+    });
+  }
 
 curatedPhotos();
