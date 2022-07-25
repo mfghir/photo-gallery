438c74241ac9c8d67f84bd5c9c1c04645692ecd9
miss-f-g <fatemeghafari98@gmail.com>
2022-07-25 09:02:12 +0430

    9.write loadMore func

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 
 const form = document.querySelector(".search-form");
+const more = document.querySelector(".more");
 let searchValue;
+
+let page = 1;
+let fetchLink;
+let currentSearch;
+
+
 
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
+more.addEventListener("click", loadMore);
+
+
 
 function updateInput(e) {
   searchValue = e.target.value;
@@ -28,11 +39,10 @@ async function fetchApi(url) {
 }
 
 function generatePictures(data) {
-
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    
+
     galleryImg.innerHTML = `
         <div class="gallery-info">
             <p>${photo.photographer}</p>
@@ -41,27 +51,35 @@ function generatePictures(data) {
         <img src=${photo.src.large}></img>`;
     gallery.appendChild(galleryImg);
   });
-
 }
 
 async function curatedPhotos() {
-  const data = await fetchApi(
-    "https://api.pexels.com/v1/curated?per_page=15&page=1"
-  );
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(query) {
   clear();
-  const data = await fetchApi(
-    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
-  );
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+  const data = await fetchApi(fetchLink);
   generatePictures(data);
 }
 
 function clear() {
   gallery.innerHTML = "";
   searchInput.value = "";
+}
+
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink);
+  generatePictures(data);
 }
 
 curatedPhotos();
